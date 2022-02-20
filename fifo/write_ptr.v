1158fb2b1c7c01673a4240616719eafed122942d
Ignatenko Oleg <oleg.ignatenko12@gmail.com>
2022-02-20 09:43:30 +0300

    Testbench for write pointer

--- fifo/write_ptr.v
+++ fifo/write_ptr.v
@@ -1,21 +1,21 @@
-module write_pointer #(parameter ADDR_SIZE=8)
-                      (input clk_i,
-                       input rst_i,
+module write_ptr #(parameter ADDR_SIZE=8)
+                  (input clk_i,
+                   input rst_i,
 
-                       input [ADDR_SIZE:0] rw_ptr_i,
-                       input inc_i, // signal to increment write pointer
+                   input [ADDR_SIZE:0] wr_ptr_2_i,
+                   input inc_i, // signal to increment write pointer
 
-                       output [ADDR_SIZE:0] ptr_o,
-                       output [ADDR_SIZE-1:0] addr_o,
-                       output fifo_full_o);
+                   output [ADDR_SIZE:0] ptr_o,
+                   output [ADDR_SIZE-1:0] addr_o,
+                   output fifo_full_o);
 
     wire _fifo_full;
-    assign _fifo_full = {~rw_ptr_i[ADDR_SIZE], rw_ptr_i[ADDR_SIZE-1:0]} == gray_cnt;
+    assign _fifo_full = {~wr_ptr_2_i[ADDR_SIZE], wr_ptr_2_i[ADDR_SIZE-1:0]} == gray_cnt;
     assign fifo_full_o = _fifo_full;
 
     // for binary and gray code counters
-    reg [ADDR_SIZE:0] bin_cnt = 0;
-    reg [ADDR_SIZE:0] gray_cnt = 0;
+    reg [ADDR_SIZE:0] bin_cnt;
+    reg [ADDR_SIZE:0] gray_cnt;
 
     wire [ADDR_SIZE:0] gray_n;
     wire [ADDR_SIZE:0] bin_n;
@@ -29,8 +29,8 @@ module write_pointer #(parameter ADDR_SIZE=8)
         end
     end
 
-    assign gray_n = (bin_cnt >> 1) ^ bin_cnt;
-    assign bin_n = bin_n + (!_fifo_full & inc_i);
+    assign gray_n = (bin_n >> 1) ^ bin_n;
+    assign bin_n = bin_cnt + (!_fifo_full & inc_i);
 
     assign ptr_o = gray_cnt;
     assign addr_o = bin_cnt[ADDR_SIZE-1:0];
---
+++ fifo/write_ptr_tb.v
@@ -0,0 +1,39 @@
+`timescale 10us/100ns
+module write_ptr_tb;
+    localparam ADDR_SIZE = 3;
+    reg clk = 0;
+    reg rst = 0;
+    reg [ADDR_SIZE:0] wr_ptr_2 = 0;
+    reg inc;
+    wire [ADDR_SIZE:0] w_ptr;
+    wire [ADDR_SIZE-1:0] w_addr;
+    wire fifo_full;
+
+    write_ptr # (.ADDR_SIZE(ADDR_SIZE)) 
+              uut(.clk_i(clk),
+                  .rst_i(rst),
+                  .wr_ptr_2_i(wr_ptr_2),
+                  .inc_i(inc),
+                  .ptr_o(w_ptr),
+                  .addr_o(w_addr),
+                  .fifo_full_o(fifo_full));
+
+    always #5 clk = !clk;
+
+    initial begin
+        $dumpfile("write_ptr_tb.vcd");
+        $dumpvars(0, write_ptr_tb);
+        # 1;
+        rst = 1;
+        wr_ptr_2 = 0;
+        inc = 1;
+    end
+
+    always @ (posedge clk) begin
+        if (fifo_full) begin
+            $display("FIFO is full, write address: %b, gray code: %b", w_addr, w_ptr);
+            $finish;
+        end
+        $display("Write address : %b, gray code: %b", w_addr, w_ptr);
+    end
+endmodule
